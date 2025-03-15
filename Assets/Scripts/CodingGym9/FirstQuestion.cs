fb1783c64ebe2f9e2de466364e96534851e66fb3
Zhanming Liu <liuzhanm@shernet.sheridancollege.ca>
2025-03-15 22:44:39 +0800

    I finished challenge one: Pointer Event, and challenge two: Cuckoo Clock Revisited.

--- Assets/Scripts/CodingGym9/FirstQuestion.cs
+++ Assets/Scripts/CodingGym9/FirstQuestion.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstQuestion : MonoBehaviour
 {
-    public GameObject image;
+    public Image image;
+
+    public GameObject imagePrefab;
+
+    public Canvas canvas;
 
     void Start()
     {
-        image.SetActive(false);
-    }
-
-    public void MouseEnter()
-    {
-        image.SetActive(true);
 
     }
 
-    public void MouseLeave()
+    public void MouseJustEnter()
     {
-        image.SetActive(false);
+        image.color = Color.red;
+    }
+
+    public void MouseJustLeave()
+    {
+        image.color = Color.white;
+    }
+
+    public void MouseJustClick()
+    {
+        Vector3 spawn = Random.insideUnitCircle * 300;
+        spawn.z = 0;
+        spawn += canvas.transform.position;
+        GameObject newImage = Instantiate(imagePrefab, spawn, Quaternion.identity);
+        //newImage.transform.parent = canvas.transform;
+        newImage.transform.SetParent(canvas.transform);
     }
 }
