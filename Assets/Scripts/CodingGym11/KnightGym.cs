29e1e70706bd37af14d939c66d5962c86537611c
Zhanming Liu <liuzhanm@shernet.sheridancollege.ca>
2025-03-29 12:48:40 -0400

    Trick Shot Remastered challenge finished.

--- Assets/Scripts/CodingGym11/KnightGym.cs
+++ Assets/Scripts/CodingGym11/KnightGym.cs
@@ -6,9 +6,18 @@ using UnityEngine;
 public class KnightGym : MonoBehaviour
 {
     SpriteRenderer sr;
+
     Animator animator;
+
+    public AnimationCurve curve;
+
+    public ParticleSystem effect;
+
     public float speed = 2;
+    public float t = 0;
+
     public bool canRun = true;
+    public bool canJump = false;
 
     public AudioClip[] audioClip;
     public AudioSource audioSource;
@@ -36,6 +45,17 @@ public class KnightGym : MonoBehaviour
         {
             transform.position += transform.right * direction * speed * Time.deltaTime;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !canJump)
+        {
+            canJump = true;
+            effect.Stop();
+            effect.Emit(5);
+        }
+        if (canJump)
+        {
+            Jump();
+        }
     }
 
     public void AttackHasFinished()
@@ -48,5 +68,21 @@ public class KnightGym : MonoBehaviour
     {
         int i = Random.Range(0, 9);
         audioSource.PlayOneShot(audioClip[i]);
+    }
+
+    public void Jump(){
+        t += Time.deltaTime;
+        Vector2 jump = transform.position;
+        jump.y = curve.Evaluate(t);
+
+        if(t > 1)
+        {
+            canJump = false;
+            t = 0;
+            effect.Stop();
+            effect.Emit(5);
+        }
+
+        transform.position = jump;
     }
 }
